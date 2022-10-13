9544098c218191bf8a4e47c151125a5575f4ac53
kamila <kamila.lopata@gmail.com>
2022-10-13 14:29:21 +0200

    Zadanie 3

--- Crawler/Crawler/Program.cs
+++ Crawler/Crawler/Program.cs
@@ -1,2 +1,35 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System.Net;
+using System.Text.RegularExpressions;
+
+namespace Crawler
+{
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            try
+            {
+                string websiteurl = args[0];
+                HttpClient client = new HttpClient();
+                var response = await client.GetStringAsync(websiteurl);
+
+                string pattern = @"([^\-\._\s\:\>][\w-\.]+[^\.])@((([\w-]+\.)+))([a-zA-Z]{2,4})";
+
+                Regex regex = new Regex(pattern);
+               // Match match = regex.Match(response);
+                MatchCollection matches = Regex.Matches(response, pattern);
+
+                foreach (Match m in matches)
+                {
+                    Console.WriteLine(m.Groups[0].Value);
+
+                }
+            }
+            catch (Exception ex)
+            {
+                string error = "error " + ex.ToString();
+            }
+        }
+    }
+}
