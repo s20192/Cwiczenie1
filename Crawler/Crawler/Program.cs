89491b50b89299f2cdd80e28373ce497fe318e93
kamila <kamila.lopata@gmail.com>
2022-10-15 17:27:37 +0200

    Zadanie4

--- Crawler/Crawler/Program.cs
+++ Crawler/Crawler/Program.cs
@@ -1,4 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using System;
 using System.Net;
 using System.Text.RegularExpressions;
 
@@ -8,27 +9,63 @@ namespace Crawler
     {
         public static async Task Main(string[] args)
         {
+            string websiteurl;
+
             try
             {
-                string websiteurl = args[0];
+                websiteurl = args[0];
+
+            } catch(Exception)
+            {
+                throw new ArgumentNullException();
+            }
+
+           CheckUrl(websiteurl);
+            
+            try
+            {
                 HttpClient client = new HttpClient();
                 var response = await client.GetStringAsync(websiteurl);
+                HashSet<string> mailMatches = new HashSet<string>();
 
                 string pattern = @"([^\-\._\s\:\>][\w-\.]+[^\.])@((([\w-]+\.)+))([a-zA-Z]{2,4})";
 
                 Regex regex = new Regex(pattern);
-               // Match match = regex.Match(response);
                 MatchCollection matches = Regex.Matches(response, pattern);
 
-                foreach (Match m in matches)
+                if(matches.Count==0)
                 {
-                    Console.WriteLine(m.Groups[0].Value);
+                    Console.WriteLine("Nie znaleziono żadnych adresów e-mail");
+                } else
+                {
+                    foreach (Match m in matches)
+                    {
+                        mailMatches.Add(m.Value);
+                    }
 
+                    foreach (string s in mailMatches)
+                    {
+                        Console.WriteLine(s);
+                    }
                 }
+
+                client.Dispose();
             }
-            catch (Exception ex)
+            catch (HttpRequestException)
             {
-                string error = "error " + ex.ToString();
+                Console.WriteLine("Błąd w czasie pobierania strony");
+            }
+        }
+
+        public static void CheckUrl(string websiteurl)
+        {
+            Uri uriResult;
+            bool isUri = Uri.TryCreate(websiteurl, UriKind.Absolute, out uriResult)
+            && (uriResult.Scheme == Uri.UriSchemeHttp || uriResult.Scheme == Uri.UriSchemeHttps);
+
+            if (!isUri)
+            {
+                throw new ArgumentException();
             }
         }
     }
